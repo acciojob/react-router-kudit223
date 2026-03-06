e4523d79bdcd495d247c2680a05762955f05568a
Udit Kumar <kudit223@gmail.com>
2026-03-06 19:15:19 +0530

    updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,20 @@
 
 import React from "react";
 import './../styles/App.css';
+import { Link } from "react-router-dom";
+import Navigation from './Navigation.jsx'
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+      <ul>
+     <li><Link to='/'>Home</Link></li>
+      <li><Link to='/about'>About</Link></li>
+      </ul>
+     
+        
+        
+        <Navigation/>
     </div>
   )
 }
